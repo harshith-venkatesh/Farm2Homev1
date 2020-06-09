fbe22e8e3ec6b4e227db1573efbf9a71a297c57a
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-09 08:32:20 +0530

    port update

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,6 @@ app.use(express.static(path.join(__dirname, "/../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started at http://localhost:5000");
 });
