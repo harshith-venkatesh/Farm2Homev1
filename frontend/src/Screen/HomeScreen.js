03e4568acffe7a9f8c8f5491fd836fd8eaff01d2
Venkatesh <Harshith.Venkatesh@CenturyLink.com>
2020-06-13 17:46:37 +0530

    update

--- frontend/src/Screen/HomeScreen.js
+++ frontend/src/Screen/HomeScreen.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { listProducts } from "../actions/productActions";
 import { logout } from "../actions/userActions";
+import StarRatings from "react-star-ratings";
 
 function HomeScreen(props) {
   const productList = useSelector((state) => state.productList);
@@ -42,7 +43,16 @@ function HomeScreen(props) {
 
               <div className="product-price">&#x20B9; {product.price} </div>
               <div className="product-rating">
-                {product.rating} Stars ({product.numReviews} reviews)
+                <StarRatings
+                  rating={product.rating}
+                  starRatedColor="yellow"
+                  //changeRating={this.changeRating}
+                  starDimension="25px"
+                  starSpacing="05px"
+                  numberOfStars={5}
+                  name="rating"
+                />
+                ({product.numReviews} reviews)
               </div>
             </div>
           </li>
